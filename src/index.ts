526e6a5276a88d00ebbff1414cf57df175ab4c50
BasSarayut <rtyhoobkk@gmail.com>
2024-11-26 13:55:10 +0700

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
 import { Elysia } from "elysia";
+import { Book, createBook, getBook, getBooks, updateBook } from "./model";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
 
+app
+    .get('/books', ()=> getBooks())
+    .get('/books/:id', ({params})=> {
+      return getBook(parseInt(params.id))
+    })
+
+    app.post('/books', ({ body, set }: { body: Book; set: any }) => {
+      const response = createBook({
+        title: body.title,
+        author: body.author,
+        description: body.description,
+        price: body.price,
+        release: body.release
+      })
+      if(response.status === 'error') {
+        set.status = 400
+        return {message: 'My Bad :('}
+      }
+
+      return {message: 'OK'}
+    })
+
+
+app.listen(3000)
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
---
+++ src/model.ts
@@ -0,0 +1,179 @@
+import { password } from "bun";
+import { Database } from "bun:sqlite";
+
+const db = new Database("mydb.sqlite");
+
+export interface Book {
+    title: string;
+    author: string;
+    description: string;
+    price: number;
+    release: number;
+  }
+
+export interface User {
+    username: string;
+    password: string;
+    // token: string;
+}
+
+const getBooks = () => {
+    try {
+        const query = db.query("SELECT * FROM books;");
+        return query.all()
+    } catch (error) {
+        console.log("error",error)
+        return {status : error}
+    }
+}
+
+const getBook = (id: number) => {
+    try {
+        const query = db.query(`SELECT * FROM books where id=$id;`);
+        return query.get({
+            $id : id
+        })
+
+        return {status : 'OK'}
+    } catch (error) {
+        console.log("error",error)
+        return {status : error}
+    }
+}
+
+const createBook = (book: Book) => {
+
+    try {
+        if(!book.title || !book.author || !book.price){
+            throw new Error('Validation Fail')
+        }
+        const query = db.query(`
+            INSERT INTO books
+            ("title","author","description","price","release")
+            VALUES ($title,$author,$description,$price,$release);`)
+
+            query.run({
+                $title: book.title,
+                $author: book.author,
+                $description: book.description,
+                $price: book.price,
+                $release: book.release
+            })
+
+            return {status : 'OK'}
+
+    } catch (error) {
+        console.log("error",error)
+        return {status : 'error',error}
+    }
+}
+
+const updateBook = (id: number, book: Book) => {
+    try {
+        const query = db.query(`
+            UPDATE books
+            SET 
+                title = $title,
+                author = $author,
+                description = $description,
+                price = $price,
+                release = $release
+            WHERE id = $id;
+        `);
+
+        query.run({
+            $id: id,
+            $title: book.title,
+            $author: book.author,
+            $description: book.description,
+            $price: book.price,
+            $release: book.release
+        });
+
+        return {status : 'OK'}
+    } catch (error) {
+        console.log("error", error);
+        return {status : error}
+    }
+};
+
+const deleteBook = (id: number) => {
+    try {
+        const query = db.query(`
+            DELETE FROM books
+            WHERE id = $id;
+        `);
+
+        query.run({
+            $id: id
+        });
+
+        return {status : 'OK'}
+    } catch (error) {
+        console.log("error", error);
+        return {status : error}
+    }
+};
+
+const createUser = (user: User) => {
+    try {
+        const query = db.query(`
+            INSERT INTO users
+            ("username","password")
+            VALUES ($username,$password);`)
+
+            query.run({
+                $username: user.username,
+                $password: user.password,
+            })
+
+            return {status : 'OK'}
+
+    } catch (error) {
+        console.log("error",error)
+        return {status : error}
+    }
+}
+
+const getUser = (user: User) => {
+    try {
+
+        const query = db.query(`
+            SELECT * FROM users 
+            WHERE username = $username AND password = $password;
+        `);
+
+        const checkUser = query.get({
+            $username: user.username,
+            $password: user.password
+        });
+
+
+        if (checkUser) {
+            return {message: true}; 
+        } else {
+            return { message: false };
+        }
+        
+    } catch (error) {
+        console.log("error", error);
+        return {status : error}
+    }
+};
+
+export {
+    getBooks,
+    getBook,
+    updateBook,
+    deleteBook,
+    getUser,
+    createBook,
+    createUser,
+}
+
+// console.log(getUser({
+//     username: 'armguyz1',
+//     password: 'asdsadasdasd'
+// }))
+
+// console.log(getBooks())
